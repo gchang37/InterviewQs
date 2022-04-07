e58a7c6ea008ccd1d93dd06c31ee706e6a8cd76b
gchang37 <gchang37@gmail.com>
2022-04-06 22:48:29 -0700

    beautified the html doc with css and js

--- Picovoice/Q2/q2.js
+++ Picovoice/Q2/q2.js
@@ -1,26 +1,24 @@
-<script>
-    $(function(){
-        /* 1. Visualizing things on Hover - See next part for action on click */
-            $('#stars li').on('mouseover', function(){
+$(function(){
+    /* 1. Visualizing things on Hover - See next part for action on click */
+        $('#stars li').on('mouseover', function(){
             var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
             
             // Now highlight all the stars that's not after the current hovered star
             $(this).parent().children('li.star').each(function(e){
                 if (e < onStar) {
-                $(this).addClass('hover');
-                }
-                else {
-                $(this).removeClass('hover');
+                    $(this).addClass('hover');
+                } else {
+                    $(this).removeClass('hover');
                 }
             });
             
             }).on('mouseout', function(){
-            $(this).parent().children('li.star').each(function(e){
-                $(this).removeClass('hover');
+                $(this).parent().children('li.star').each(function(e){
+                    $(this).removeClass('hover');
             });
-            });
-        /* 2. Action to perform on click */
-            $('#stars li').on('click', function(){
+        });
+    /* 2. Action to perform on click */
+        $('#stars li').on('click', function(){
             var onStar = parseInt($(this).data('value'), 10); // The star currently selected
             var stars = $(this).parent().children('li.star');
             
@@ -32,10 +30,26 @@
                 $(stars[i]).addClass('selected');
             }
             alert("Thank you for your feedback for  "+ onStar + " star(s) rating. Please share your thoughts via email because this page is not hooked up to any servicing backend")
-            
-            });
+        
+        });
 
-            /*image slider*/
+        /*image slider*/
+        var imgSlideIndex = 1;
+        function showImage(pos){
+            var x = document.getElementsByClassName("slides");
+            if(pos > x.length){ imgSlideIndex = 1;}
+            if(pos < 1) {imgSlideIndex = x.length;}
+            for(var i = 0; i < x.length; i++){
+                x[i].style.display = "none";
+            }
+            x[imgSlideIndex-1].style.display="block";
+        }
+        showImage(imgSlideIndex);
 
-    });
-</script>
+        $("button").click(function(){
+            var val = parseInt( $(this).data('value'), 10);
+            // alert("btn click detected and value is " + val);
+            showImage(imgSlideIndex += val);            
+        });
+});
+
